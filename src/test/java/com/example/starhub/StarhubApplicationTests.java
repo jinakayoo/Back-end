93e0969470a6b1e3eb35f7d9f40338678e52795d
pyounani <yndbsk9372@gmail.com>
2023-10-10 16:47:07 +0900

    [init] git init

---
+++ src/test/java/com/example/starhub/StarhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
